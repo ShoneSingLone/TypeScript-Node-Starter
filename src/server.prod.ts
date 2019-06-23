51f76cb1f8e233d4e4602b38ca582aad3e4b0dcd
ShoneSingLone <ShoneSingLone@gmail.com>
2019-06-23 19:53:35 +0800

    添加prod production启动

---
+++ src/server.prod.ts
@@ -0,0 +1,23 @@
+process.env.NODE_ENV = "production";
+import errorHandler from "errorhandler";
+import app from "./app";
+console.log(process.env.NODE_ENV);
+
+/**
+ * Error Handler. Provides full stack - remove for production
+ */
+app.use(errorHandler());
+
+/**
+ * Start Express server.
+ */
+const server = app.listen(app.get("port"), () => {
+  console.log(
+    "  App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env")
+  );
+  console.log("  Press CTRL-C to stop\n");
+});
+
+export default server;
